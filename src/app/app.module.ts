b0ffb87d5518187d06cf8a7eee4238880fff405a
Juancho <yojuan@gmail.com>
2021-06-19 04:47:12 -0500

    1er archivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule
